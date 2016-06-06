d91ff6766a59fa04ca32868f1fad5246ae20c79c
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-06-06 17:35:16 +0100

    add cmdline options

--- dev/scripts/git-subproject-recursive-checkout.sh
+++ dev/scripts/git-subproject-recursive-checkout.sh
@@ -2,14 +2,47 @@
 
 # set configuration vars
 
+CMD_OPTS=":b:h"
 BRANCH="master"
 
-if [ -z "$1" ]; then 
-  echo "warning: repo branch was not provided" 
-  echo "warning: using default fallback url: ${BRANCH}"
-else
-  BRANCH="$1"
-fi
+
+read -r -d '' HELP_STR <<'EOF'
+Usage: scriptname -h -b [branch]
+
+Output Options:
+  -b [branch]       Use this 'branch' name, otherwise 'master'.
+  -h                This help screen.
+EOF
+
+
+# parse command-line options
+
+OPT_B_USED=false
+
+while getopts "${CMD_OPTS}" opt; do
+  case $opt in
+    b)
+      ${OPT_B_USED} && echo "Option: -$opt used more than once." >&2 && exit 1
+      BRANCH=${OPTARG}
+      OPT_B_USED=true
+      ;;
+    h)
+      echo "${HELP_STR}" >&2
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG." >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+#
 
 GIT_PROJECTS=( $(find . -iname ".git" | xargs) )
 
--- dev/scripts/llvm-git-clone.sh
+++ dev/scripts/llvm-git-clone.sh
@@ -2,14 +2,45 @@
 
 # set configuration vars
 
+CMD_OPTS=":r:h"
 REPO="http://llvm.org/git"
 
-if [ -z "$1" ]; then 
-  echo "warning: repo url was not provided" 
-  echo "warning: using default fallback url: ${REPO}"
-else
-  REPO="$1"
-fi
+
+read -r -d '' HELP_STR <<'EOF'
+Usage: scriptname -h -r [repo URL]
+
+Output Options:
+  -r [repo URL]     Use this as a repo URL, otherwise default value.
+  -h                This help screen.
+EOF
+
+
+# parse command-line options
+
+OPT_R_USED=false
+
+while getopts "${CMD_OPTS}" opt; do
+  case $opt in
+    r)
+      ${OPT_R_USED} && echo "Option: -$opt used more than once." >&2 && exit 1
+      echo ${OPTARG}
+      REPO=${OPTARG}
+      OPT_R_USED=true
+      ;;
+    h)
+      echo "${HELP_STR}" >&2
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG." >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
 
 #
