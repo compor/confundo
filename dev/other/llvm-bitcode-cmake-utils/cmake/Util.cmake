514fded63cea5e658d196ad79797405bdec6f99a
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-06-14 18:08:50 +0100

    add LLVM opt pass target

--- dev/other/llvm-bitcode-cmake-utils/cmake/Util.cmake
+++ dev/other/llvm-bitcode-cmake-utils/cmake/Util.cmake
@@ -1,4 +1,5 @@
 
+# LLVM cmake utils
 
 function(create_hierarchy_dir ROOT_DIR)
   foreach(DIR ${ARGN})
@@ -6,21 +7,10 @@ function(create_hierarchy_dir ROOT_DIR)
   endforeach()
 endfunction()
 
- 
-function(generate_bitcode TRGT BC_TRGT)
-  set(BC_DIR "${CMAKE_CURRENT_BINARY_DIR}/bitcode")
-  set(BC_PLAIN_DIR "${BC_DIR}/plain")
-  set(BC_PRE_DIR "${BC_DIR}/prelink")
-  set(BC_POST_DIR "${BC_DIR}/postlink")
-  file(MAKE_DIRECTORY "${BC_DIR}")
-  file(MAKE_DIRECTORY "${BC_PLAIN_DIR}")
-  file(MAKE_DIRECTORY "${BC_PRE_DIR}")
-  file(MAKE_DIRECTORY "${BC_POST_DIR}")
 
-  set_target_properties(${TRGT} PROPERTIES BITCODE_ROOT_DIR "${BC_DIR}")
-  set_target_properties(${TRGT} PROPERTIES BITCODE_PLAIN_DIR "${BC_PLAIN_DIR}")
-  set_target_properties(${TRGT} PROPERTIES BITCODE_PRELINK_DIR "${BC_PRE_DIR}")
-  set_target_properties(${TRGT} PROPERTIES BITCODE_POSTLINK_DIR "${BC_POST_DIR}")
+function(attach_bitcode_target BITCODE_TRGT TRGT)
+  set(BC_DIR "${CMAKE_CURRENT_BINARY_DIR}/${BITCODE_TRGT}")
+  file(MAKE_DIRECTORY "${BC_DIR}")
 
   #
 
@@ -44,11 +34,11 @@ function(generate_bitcode TRGT BC_TRGT)
   foreach(DIRECTORY ${INC_DIRS})
     list(APPEND SRC_INCLUDES -I${DIRECTORY})
   endforeach()
-   
+
   set(SRC_COMPILE_FLAGS "")
   get_property(SRC_COMPILE_FLAGS TARGET ${TRGT} PROPERTY COMPILE_FLAGS)
 
-  # 
+  #
 
   get_property(SRCS TARGET ${TRGT} PROPERTY SOURCES)
   set(BCFILES "")
@@ -56,30 +46,89 @@ function(generate_bitcode TRGT BC_TRGT)
   foreach(SRC_FILE ${SRCS})
     get_filename_component(OUTFILE ${SRC_FILE} NAME)
     get_filename_component(INFILE ${SRC_FILE} ABSOLUTE)
-    set(BCFILE "${SRC_FILE}.bc")
-    set(FULL_BCFILE "${BC_PLAIN_DIR}/${BCFILE}")
+    set(BCFILE "${OUTFILE}.bc")
+    set(FULL_BCFILE "${BC_DIR}/${BCFILE}")
 
+    # TODO add debug flag
     add_custom_command(OUTPUT ${FULL_BCFILE}
-      COMMAND clang -emit-llvm ${SRC_DEFS} ${SRC_COMPILE_FLAGS} ${SRC_INCLUDES}
-      -c ${INFILE} -o ${FULL_BCFILE}
+      COMMAND clang -emit-llvm
+      ${SRC_DEFS} ${SRC_COMPILE_FLAGS} ${SRC_INCLUDES}
+      -c ${INFILE}
+      -o ${FULL_BCFILE}
       DEPENDS ${INFILE}
       IMPLICIT_DEPENDS CXX ${INFILE}
       COMMENT "Building LLVM bitcode ${BCFILE}"
       VERBATIM)
-    set_property(DIRECTORY APPEND 
-      PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${FULL_BCFILE})
 
-    #add_custom_target(${BCFILE} DEPENDS ${OUTFILE})
+    set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES 
+      ${FULL_BCFILE})
+    set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${BCDIR})
 
-    ## keep track of every bitcode file we need to create
     list(APPEND BCFILES ${FULL_BCFILE})
   endforeach()
 
-  add_custom_target(${BC_TRGT} DEPENDS "${BCFILES}")
 
-  #set_target_properties(${BC_TRGT} PROPERTIES DEPENDS "${BCFILES}")
-  set_target_properties(${TRGT} PROPERTIES BITCODE "${BCFILES}")
+  # setup custom target
 
-  add_dependencies(${TRGT} ${BC_TRGT})
+  add_custom_target(${BITCODE_TRGT} DEPENDS "${BCFILES}")
+
+  set_target_properties(${BITCODE_TRGT} PROPERTIES BITCODE_ROOT_DIR "${BC_DIR}")
+  set_target_properties(${BITCODE_TRGT} PROPERTIES BITCODE_FILES "${BCFILES}")
+
+  add_dependencies(${TRGT} ${BITCODE_TRGT})
 endfunction()
+
+
+function(attach_opt_pass_target OPT_TRGT BITCODE_TRGT LIB_LOCATION
+    CMDLINE_OPTION)
+  set(BC_DIR "${CMAKE_CURRENT_BINARY_DIR}/${OPT_TRGT}")
+  file(MAKE_DIRECTORY "${BC_DIR}")
+
+  set(LIB_OPTION "")
+  if(NOT ${LIB_LOCATION} STREQUAL "")
+    set(LIB_OPTION "-load ${LIB_LOCATION}")
+  endif()
+  set(CMDLINE_OPTION "-${CMDLINE_OPTION}")
+
+  # TODO other opt options for debug
+  set(OPT_PASS_OPTIONS "")
+
+  set(BCFILES "")
+  get_property(INBCFILES TARGET ${BITCODE_TRGT} PROPERTY BITCODE_FILES)
+
+  foreach(INBCFILE ${INBCFILES})
+    get_filename_component(OUTFILE ${INBCFILE} NAME)
+    get_filename_component(INFILE ${INBCFILE} ABSOLUTE)
+    set(BCFILE "${OUTFILE}-${OPT_TRGT}")
+    set(FULL_BCFILE "${BC_DIR}/${BCFILE}")
+
+    # TODO add other flags
+    add_custom_command(OUTPUT ${FULL_BCFILE}
+      COMMAND opt
+      ${LIB_OPTION}
+      ${CMDLINE_OPTION}
+      ${INFILE}
+      -o ${FULL_BCFILE}
+      DEPENDS ${INFILE}
+      IMPLICIT_DEPENDS CXX ${INFILE}
+      COMMENT "Building LLVM bitcode ${BCFILE}"
+      VERBATIM)
+
+    set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES 
+      ${FULL_BCFILE})
+    set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${BCDIR})
+
+    list(APPEND BCFILES ${FULL_BCFILE})
+  endforeach()
+
+
+  # setup custom target
+
+  add_custom_target(${OPT_TRGT} DEPENDS "${BCFILES}")
+
+  set_target_properties(${OPT_TRGT} PROPERTIES BITCODE_FILES "${BCFILES}")
+
+  add_dependencies(${BITCODE_TRGT} ${OPT_TRGT})
+endfunction()
+
 
